56674069515f4f50db21fae66721410415ef2863
piriyadharisini <31730822+piriyadharisini@users.noreply.github.com>
2017-09-14 12:46:54 +0530

    Add files via upload

---
+++ repeat.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void printTwoElements(int arr[], int size)
+{
+	int i;
+	printf("\n The repeating element is");
+
+	for(i = 0; i < size; i++)
+	{
+		if(arr[abs(arr[i])-1] > 0)
+			arr[abs(arr[i])-1] = -arr[abs(arr[i])-1];
+		else
+			printf(" %d ", abs(arr[i]));
+	}
+
+	
+}
+
+
+int main()
+{
+	int arr[] = {7, 3, 4, 5, 5, 6, 2};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	printTwoElements(arr, n);
+	return 0;
